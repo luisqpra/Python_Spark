2f1b9f8ae21ddcad3b0b60506c454fc528eec07b
Luis Quiroz <luisqpra@gmail.com>
2023-12-17 12:07:32 -0500

    feat() - C2L1: creacion de una session de spark, iniciar proceso

---
+++ recursos/C2L1.py
@@ -0,0 +1,11 @@
+# SparkSession
+
+import findspark
+from pyspark.sql import SparkSession
+
+findspark.init()
+
+spark = SparkSession.builder.master(
+    "local[*]").appName('Curso Pyspark').getOrCreate()
+
+spark.close()
