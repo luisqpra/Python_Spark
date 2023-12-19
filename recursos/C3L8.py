8567ed061256330f78dbc09099e9cae35c69fc2c
Luis Quiroz <luisqpra@gmail.com>
2023-12-19 16:49:01 -0500

    feat() - Transformacion de RDD con repartition

---
+++ recursos/C3L8.py
@@ -0,0 +1,23 @@
+# Transformaciones: función reduceByKey
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+sc = spark.sparkContext
+
+rdd = sc.parallelize(
+    [('casa', 2),
+     ('parque', 1),
+     ('que', 5),
+     ('casa', 1),
+     ('escuela', 2),
+     ('casa', 1),
+     ('que', 1)]
+)
+
+rdd.collect()
+
+rdd_reduciodo = rdd.reduceByKey(
+    lambda x, y: x + y
+    )
+
+rdd_reduciodo.collect()
