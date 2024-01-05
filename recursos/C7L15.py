37c568adc02c0de9bb67793a93175b95347a7846
Luis Quiroz <luisqpra@gmail.com>
2024-01-05 16:16:31 -0500

    feat() - left_semi, union de dataframes

---
+++ recursos/C7L15.py
@@ -0,0 +1,11 @@
+# Left Semi Join
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder.getOrCreate()
+
+empleados = spark.read.parquet('./data/empleados/')
+
+departamentos = spark.read.parquet('./data/departamentos/')
+
+empleados.join(departamentos, col('num_dpto') == col('id'), 'left_semi').show()
